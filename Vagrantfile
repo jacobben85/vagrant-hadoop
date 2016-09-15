01a7cdcd3f23574507e7c406ef2beef1f9d482a1
Jacob Benny John <jacobben85@gmail.com>
2016-09-14 23:15:09 -0700

    Adding the initial file set

---
+++ Vagrantfile
@@ -0,0 +1,25 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+  config.vm.box = "ubuntu/trusty64"
+  config.vm.network "private_network", ip: "192.168.56.110"
+  config.vm.hostname = "hadoop.vagrant-local.jbj"
+
+  config.vm.provider "virtualbox" do |v|
+    v.memory = 2048
+    v.cpus = 1
+    v.name = "hadoop"
+  end
+
+   port1 = 5044
+
+   config.vm.network(:forwarded_port, guest: port1, host: port1)
+
+  config.vm.provision "shell",
+    path: "scripts/bootstrap.sh"
+
+  config.vm.provision "shell",
+    path: "scripts/startup.sh",
+  run: "always"
+end
---
+++ scripts/bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "Installing Java 8"
+
+apt-get --assume-yes update
+apt-get install -y python-software-properties debconf-utils
+add-apt-repository ppa:webupd8team/java -y
+apt-get --assume-yes update
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+apt-get install -y oracle-java8-installer
+apt-get install -y oracle-java8-set-default
+
+echo "Java 8 installation complete"
+
+apt-get --assume-yes update
+apt-get install -y htop
+apt-get install -y maven
+apt-get install -y nmap
+apt-get install -y openssh-server
